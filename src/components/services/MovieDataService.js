ca6db685c02a4d4ab1f7e80183e1b6570589135c
DThanh1508 <athanh2002kt@gmail.com>
2022-08-15 07:01:31 -0400

    chang api: movies=> movies

--- src/components/services/MovieDataService.js
+++ src/components/services/MovieDataService.js
@@ -1,25 +1,31 @@
 import http from "../../http-common";
 class MovieDataService {
     getAll() {
-        return http.get("/movies");
+        return http.get("/movie");
     }
     get(id) {
-        return http.get(`/movies/${id}`);
+        return http.get(`/movie/${id}`);
     }
     create(data) {
-        return http.post("/movies", data);
+        return http.post("/movie", data);
     }
     update(id, data) {
-        return http.put(`/movies/${id}`, data);
+        return http.put(`/movie/${id}`, data);
     }
     delete(id) {
-        return http.delete(`/movies/${id}`);
+        return http.delete(`/movie/${id}`);
     }
     deleteAll() {
-        return http.delete(`/movies`);
+        return http.delete(`/movie`);
     }
     findByTitle(title) {
-        return http.get(`/movies?title=${title}`);
+        return http.get(`/movie?title=${title}`);
+    }
+    getActive() {
+        return http.get(`/movie/active`);
+    }
+    deleteActiveDelete() {
+        return http.get(`/movie/activeDelete`);
     }
 }
 export default new MovieDataService();
