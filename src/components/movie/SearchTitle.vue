a144f86a3ffcc94912fe4ed9f7e60ca50baf9b3e
DThanh1508 <athanh2002kt@gmail.com>
2022-08-15 07:02:12 -0400

    create SearchTitle

---
+++ src/components/movie/SearchTitle.vue
@@ -0,0 +1,14 @@
+<!--<template>-->
+
+<!--</template>-->
+
+<!--<script>-->
+<!--// import MovieDataService from "@/components/services/MovieDataService";-->
+<!--export default {-->
+<!--  name: "SearchTitle"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
